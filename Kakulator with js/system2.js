013dd01150d708eb9f1dc94d562ce89d31bd42fa
I Putu Mahendra Arya Valentino <138369170+Aryndra@users.noreply.github.com>
2023-07-06 01:10:49 +0800

    kakulator

---
+++ Kakulator with js/system2.js
@@ -0,0 +1,40 @@
+let input = document.getElementById("inputBox");
+let buttons = document.querySelectorAll("button");
+
+let string = "";
+let arr = Array.from(buttons);
+arr.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (e.target.innerHTML == "=") {
+      string = eval(string);
+      input.value = string;
+    } else if (e.target.innerHTML == "AC") {
+      string = "";
+      input.value = string;
+    } else if (e.target.innerHTML == "DEL") {
+      string = string.substring(-1, string.length - 1);
+      input.value = string;
+    } else {
+      string += e.target.innerHTML;
+      input.value = string;
+    }
+  });
+});
+
+var password = "";
+
+function addToPassword(number) {
+  password += number;
+  document.getElementById("password").value = password;
+}
+
+function checkPassword() {
+  if (password === "01010") {
+    // Ganti "123456" dengan sandi yang Anda inginkan
+    window.location.href = "kakulator.html";
+  } else {
+    alert("Sandi salah. Coba lagi.");
+    password = "";
+    document.getElementById("password").value = "";
+  }
+}
